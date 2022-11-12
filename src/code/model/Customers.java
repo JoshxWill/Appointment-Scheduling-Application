f2ddac350feb1d91ec7bf5e2fe18f6ab13d48018
Joshua <jwi3411@wgu.edu>
2022-11-12 14:32:39 -0600

    Sixth save

--- src/code/model/Customers.java
+++ src/code/model/Customers.java
@@ -1,5 +1,6 @@
 package code.model;
 
+import code.dao.DataBaseCustomers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -26,6 +27,8 @@ public class Customers {
         this.division = division;
         this.country = country;
     }
+    public Customers(int ID, String name, String address, String postal, String phone, String country, int divisionID){ }
+
     public Customers(int customerID, String customerName, String address, String postalCode, String phone, FirstLevelDivisions division){
         this.customerID= customerID;
         this.customerName = customerName;
@@ -33,6 +36,138 @@ public class Customers {
         this.postalCode = postalCode;
         this.phone = phone;
         this.division = String.valueOf(division);
+    }
+
+    public Customers(int customerID, String customerName, String address, String postal, String phone, int divisionID){}
+
+    /**Return all Customers from database**/
+    public static ObservableList<Customers> getAllCustomers(){
+        return DataBaseCustomers.getAllCustomers();
+    }
+
+    public static void addCustomer(Customers newCustomer){
+    }
+
+    /**Getter for customerID**/
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    /**
+     *Setter for customerID
+     * @param ID
+     **/
+    public void setID(int ID){
+        this.customerID = ID;
+    }
+
+    /**Getter for customerName**/
+    public String getCustomerName(){
+        return customerName;
+    }
+
+    /**
+     * Setter for customerName
+     * @param customerName
+     */
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    /**Getter for Customer Address**/
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Setter for Customer Address
+     * @param address
+     */
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**Getter for Customer postalCode**/
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+
+    /**
+     * Setter for Customer Postal Code
+     * @param postalCode
+     */
+
+    public void setPostalCode(String postalCode){
+        this.postalCode = postalCode;
+    }
+
+    /**Getter for Customer Phone**/
+
+    public String getPhone() {
+        return phone;
+    }
+
+    /**Setter for Customer Phone
+     * @param phone
+     */
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**Getter for Division**/
+
+    public String getDivision() {
+        return division;
+    }
+
+    /**
+     * Setter for Division
+     * @param division
+     */
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    /**Getter for Division ID**/
+
+    public int getDivisionID() {
+        return divisionID;
+    }
+
+    /**
+     * Setter for Division ID
+     * @param divisionID
+     */
+
+    public void setDivisionID(int divisionID) {
+        this.divisionID = divisionID;
+    }
+
+    /**Getter for Customer Country**/
+    public String getCountry() {
+        return country;
+    }
+
+    /**Setter for Customer Country**/
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public ObservableList<Appointments> getAssociatedAppointments(){
+        return associatedAppointments;
+    }
+
+    /**Override toString method for Menu box**/
+    @Override
+    public String toString(){
+        return("#" + Integer.toString(customerID) + " " + customerName);
     }
 
 }
