8ac3da4d67fb992b092021b47c0c8358a9e164e6
Joshua <jwi3411@wgu.edu>
2022-11-16 14:58:33 -0600

    Ninth save

--- src/code/controller/OverallReportController.java
+++ src/code/controller/OverallReportController.java
@@ -1,28 +1,93 @@
 package code.controller;
 
-public class OverallReportController {
+import code.dao.DataBaseAppts;
+import code.model.Appointments;
+import code.model.MonthlyReports;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+/**
+ * Overall Appointment Report
+ *
+ * @author Joshua Williams
+ */
+public class OverallReportController implements Initializable {
+    @FXML
+    private TableView<MonthlyReports> OARTable;
     @javafx.fxml.FXML
-    private TableColumn OverallReportMonthColumn;
+    private TableColumn<Appointments, String> OverallReportMonthColumn;
     @javafx.fxml.FXML
-    private TableColumn OverallReportTypeColumn;
+    private TableColumn<Appointments, String> OverallReportTypeColumn;
     @javafx.fxml.FXML
-    private TableColumn OverallReportCountColumn;
+    private TableColumn<Appointments, Integer> OverallReportCountColumn;
     @javafx.fxml.FXML
-    private ComboBox OARMonth;
+    private ComboBox<String> OARMonth;
     @javafx.fxml.FXML
     private Button Results;
     @javafx.fxml.FXML
     private Button BackBtn;
 
+    Parent scene;
+    Stage stage;
+
     @javafx.fxml.FXML
     public void OARMonthMenu(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
-    public void SeeMonthBtn(ActionEvent actionEvent) {
+    /**
+     * Activate Results Button to show selected month report
+     *
+     * @param actionEvent
+     * @throws SQLException
+     */
+    @FXML
+    public void SeeMonthBtn(ActionEvent actionEvent) throws SQLException {
+        String monthSelect = OARMonth.getSelectionModel().getSelectedItem().toUpperCase();
+        OARTable.setItems(DataBaseAppts.getApptByMonthAndType(monthSelect));
+        System.out.println("Results Button Activated");
     }
 
+    /**
+     * Returns To Main Report Screen
+     *
+     * @param actionEvent
+     * @throws IOException
+     */
     @javafx.fxml.FXML
-    public void ReturnReportScreen(ActionEvent actionEvent) {
+    public void ReturnReportScreen(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Warning!");
+        alert.setContentText("You sure you want to return?");
+        alert.showAndWait();
+
+        stage = (Stage)((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainReportScreen.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        OverallReportMonthColumn.setCellValueFactory(new PropertyValueFactory<>("month"));
+        OverallReportTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        OverallReportCountColumn.setCellValueFactory(new PropertyValueFactory<>("count"));
+
+        ObservableList<String> months = FXCollections.observableArrayList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        OARMonth.setItems(months);
+        OARMonth.getSelectionModel().selectFirst();
     }
 }
--- src/code/controller/UserScheduleReportController.java
+++ src/code/controller/UserScheduleReportController.java
@@ -1,36 +1,103 @@
 package code.controller;
 
-public class UserScheduleReportController {
+import code.dao.DataBaseAppts;
+import code.dao.DataBaseUsers;
+import code.model.Appointments;
+import code.model.Customers;
+import code.model.Users;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+
+/**
+ * User Schedule Report
+ *
+ * @author Joshua Williams
+ */
+public class UserScheduleReportController implements Initializable {
     @javafx.fxml.FXML
-    private TableColumn USRApptColumn;
+    private TableColumn<Appointments, Integer> USRApptColumn;
     @javafx.fxml.FXML
-    private TableColumn USRTitleColumn;
+    private TableColumn<Appointments, String> USRTitleColumn;
     @javafx.fxml.FXML
-    private TableColumn USRTypeColumn;
+    private TableColumn<Appointments, String> USRTypeColumn;
     @javafx.fxml.FXML
-    private TableColumn USRDescriptionColumn;
+    private TableColumn<Appointments, String> USRDescriptionColumn;
     @javafx.fxml.FXML
-    private TableColumn USRStartDTColumn;
+    private TableColumn<Appointments, Date> USRStartDTColumn;
     @javafx.fxml.FXML
-    private TableColumn USREndDTColumn;
+    private TableColumn<Appointments, Date> USREndDTColumn;
     @javafx.fxml.FXML
-    private TableColumn USRCustomerIDColumn;
+    private TableColumn<Customers, Integer> USRCustomerIDColumn;
     @javafx.fxml.FXML
-    private ComboBox SelectUser;
+    private ComboBox<Users> SelectUser;
     @javafx.fxml.FXML
     private Button USRResults;
     @javafx.fxml.FXML
     private Button USRBackBtn;
+    @javafx.fxml.FXML
+    private TableView<Appointments> userRT;
+
+    Parent scene;
+    Stage stage;
 
     @javafx.fxml.FXML
     public void SelectUserMenu(ActionEvent actionEvent) {
     }
 
+    /**
+     * Activate Results Button to show selected User
+     *
+     * @param actionEvent
+     */
     @javafx.fxml.FXML
     public void USRResultsBtn(ActionEvent actionEvent) {
+        int userSelect = SelectUser.getSelectionModel().getSelectedItem().getUserID();
+        userRT.setItems(DataBaseUsers.getAppointmentsByUser(userSelect));
+        System.out.println("Results Button Activated");
     }
 
+    /**
+     * Returns to Main Report Screen
+     *
+     * @param actionEvent
+     * @throws IOException
+     */
     @javafx.fxml.FXML
-    public void ReturnReportScreen(ActionEvent actionEvent) {
+    public void ReturnReportScreen(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Warning!");
+        alert.setContentText("You sure you want to return?");
+        alert.showAndWait();
+
+        stage = (Stage)((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainReportScreen.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        userRT.setItems(DataBaseAppts.getAllAppointments());
+        SelectUser.setItems(DataBaseUsers.getAllUsers());
+        SelectUser.getSelectionModel().selectFirst();
+
+        USRApptColumn.setCellValueFactory(new PropertyValueFactory<>("appointmentID"));
+        USRTitleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        USRDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        USRTypeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        USRStartDTColumn.setCellValueFactory(new PropertyValueFactory<>("start"));
+        USREndDTColumn.setCellValueFactory(new PropertyValueFactory<>("end"));
+        USRCustomerIDColumn.setCellValueFactory(new PropertyValueFactory<>("customerID"));
     }
 }
